44995dda2e4b360180f921e116645e6976fcd235
Ganesh <garunagiri@sapient.com>
2016-09-10 16:58:29 +0530

    SecondTest refactor

--- numbertotextconverter/src/test/java/com/spart/tdd/TextConverterTest.java
+++ numbertotextconverter/src/test/java/com/spart/tdd/TextConverterTest.java
@@ -2,21 +2,36 @@ package com.spart.tdd;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextConverterTest {
+	
+	private TextConverter converter;
+
+	@Before
+	public void setup()
+	{
+		 converter=new TextConverter();
+	}
 
 	@Test
 	public void testNumberOneToText() {
-		TextConverter converter=new TextConverter();
+		
 		assertEquals("one",converter.ConvertToText(1));
 	}
 	
 	@Test
 	public void testNumberTwoToText() {
-		TextConverter converter=new TextConverter();
+		
 		assertEquals("two",converter.ConvertToText(2));
 	}
 	
+	@After
+	public void cleanup()
+	{
+		converter=null;
+	}
 
 }
