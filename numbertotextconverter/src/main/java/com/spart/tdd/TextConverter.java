4e78f78dd446a86eac1ea9319659bf4bb21ccb03
Ganesh <garunagiri@sapient.com>
2016-09-10 16:56:03 +0530

    Second Test case Green

--- numbertotextconverter/src/main/java/com/spart/tdd/TextConverter.java
+++ numbertotextconverter/src/main/java/com/spart/tdd/TextConverter.java
@@ -2,8 +2,11 @@ package com.spart.tdd;
 
 public class TextConverter {
 
+	public static final String[] numberNames ={"zero","one","two"};
+	
 	public String ConvertToText(int inputNumber) {
-		return "one";
+				
+		return numberNames[inputNumber];
 	}
 
 }
